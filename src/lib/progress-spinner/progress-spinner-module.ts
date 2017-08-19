868c30b26586d917f388faf37b204a171802da10
crisbeto <crisbeto@abv.bg>
2017-08-19 19:12:24 +0200

    feat(progress-spinner): switch to css-based animation
    Reworks the progress spinner to use a CSS-based indeterminate animation. For IE and Edge where we can't pull of the animation reliably, it falls back to a non-spec animation.

--- src/lib/progress-spinner/progress-spinner-module.ts
+++ src/lib/progress-spinner/progress-spinner-module.ts
@@ -5,28 +5,16 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
 import {NgModule} from '@angular/core';
 import {MdCommonModule} from '@angular/material/core';
-import {
-  MdProgressSpinner,
-  MdSpinner,
-  MdProgressSpinnerCssMatStyler,
-} from './progress-spinner';
+import {MdProgressSpinner, MdSpinner} from './progress-spinner';
 
 
 @NgModule({
   imports: [MdCommonModule],
-  exports: [
-    MdProgressSpinner,
-    MdSpinner,
-    MdCommonModule,
-    MdProgressSpinnerCssMatStyler
-  ],
-  declarations: [
-    MdProgressSpinner,
-    MdSpinner,
-    MdProgressSpinnerCssMatStyler
-  ],
+  exports: [MdProgressSpinner, MdSpinner, MdCommonModule],
+  declarations: [MdProgressSpinner, MdSpinner],
 })
-export class MdProgressSpinnerModule {}
+class MdProgressSpinnerModule {}
+
+export {MdProgressSpinnerModule};
